525f2bb1e6e6834189684a1d270006c84c206007
akingw <adam.king.w@gmail.com>
2023-12-07 13:03:26 -0700

    Part2
    No fighting with regex this time

--- Day3.R
+++ Day3.R
@@ -6,13 +6,13 @@ input2 = gsub("\\.","a",input)
 
 # [:punct:] doesn't match "=", despite the documentation for stringr 
 # saying otherwise...
-str_locate("=test!","[:punct:]")
-str_locate("=test!","[:punct:]|=")
+## str_locate("=test!","[:punct:]")
+## str_locate("=test!","[:punct:]|=")
 
 # Make a list of the symbols used:
-arr = gsub("\\d+","",gsub("\\.","",input)) %>% paste0(collapse="") %>%
-  str_split(pattern="")
-used = arr[[1]] %>% unique()
+## arr = gsub("\\d+","",gsub("\\.","",input)) %>% paste0(collapse="") %>%
+##   str_split(pattern="")
+## used = arr[[1]] %>% unique()
 
 #markers = str_locate_all(input2,"[:punct:]")
 #markers = str_locate_all(input2,"[:punct:]|=")
@@ -36,8 +36,8 @@ for(line in markers){
 
 i = 0
 sum = 0
-added = tibble(val  =numeric(),
-               row = numeric())
+## added = tibble(val  =numeric(),
+##                row = numeric())
 numbers = str_locate_all(input2,"\\d+")
 for(line in numbers){
   i = i + 1
@@ -57,8 +57,7 @@ for(line in numbers){
         nrow()
       if(nr > 0){
         # Added to spot check after running
-        added = added %>% add_row(val = num, row = i)
-#        print(paste0("Adding ",num," to sum"))
+        ## added = added %>% add_row(val = num, row = i)
         matched = T
         sum = sum + num
       }
@@ -73,3 +72,48 @@ print(sum)
 #470101 too low :/
 #835344 too high
 #782504 too high
+
+# Part 2
+
+i = 0
+numbers = str_locate_all(input,"\\d+")
+numberSpots = tibble(xRange = numeric(),
+                     yRange = numeric(),
+                     val = numeric())
+for(line in numbers){
+  i = i + 1
+  if(nrow(line)==0){next}
+  for(j in 1:nrow(line)){
+    # This is the number that a ring will be created around.
+    num = as.numeric(str_sub(
+      input[i], line[j,1], line[j,2]))
+    xRange = c((line[j,1]-1):(line[j,2]+1))
+    yRange = c((i-1):(i+1))
+    ranges = expand_grid(xRange,yRange) %>%
+      mutate(val = num)
+    # builds out coordinates that a gear can be in that
+    # are adjacent to a number
+    numberSpots = numberSpots %>%
+      add_row(ranges)
+  }
+}
+
+gears = str_locate_all(input,"\\*")
+sumPower = 0
+i = 0
+for(line in gears){
+  i = i + 1
+  if(nrow(line)==0){next}
+  for(j in 1:nrow(line)){
+    gearX = line[j,1]
+    gearY = i
+    adjacentNums = numberSpots %>%
+      filter(xRange == gearX,
+             yRange == gearY)
+    if(nrow(adjacentNums)==2){
+      power = prod(adjacentNums$val)
+      sumPower = sumPower + power
+    }
+  }
+}
+print(sumPower)
