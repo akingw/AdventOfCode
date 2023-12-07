f444e8566ded31657272b62c2628ee936a5d532a
akingw <adam.king.w@gmail.com>
2023-12-06 17:44:20 -0700

    Part 2 solution

--- Day2.R
+++ Day2.R
@@ -30,3 +30,33 @@ for(game in input){
 }
 
 print(count)
+
+count = 0
+
+for(game in input){
+  colors = tibble(
+    color = c('red',
+              'green',
+              'blue'),
+    limit = c(0, 0, 0)
+  )
+  #possible = T
+  gameNum = as.numeric(str_extract(game,"\\d+"))
+  for(i in 1:nrow(colors)){
+    col = colors$color[i]
+    cubes = str_extract_all(game,paste0("\\d+ ",col))[[1]]
+    for(j in cubes){
+      lim = colors$limit[i]
+      n = as.numeric(str_extract(j,"\\d+"))
+      if(n>lim){
+        colors = colors %>%
+          mutate(limit = case_when(
+            color == col ~ n,
+            T ~ limit
+          ))
+      }
+    }
+  }
+  count = count + prod(colors$limit)
+}
+print(count)
