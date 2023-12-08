d3a63e76365f21e7a386ec162ffaf6bcb2e8810b
akingw <adam.king.w@gmail.com>
2023-12-08 14:04:44 -0700

    Update Day5.R
    Progress, think the issue is using <= and >= instead of < or > in some spots

--- Day5.R
+++ Day5.R
@@ -11,7 +11,9 @@ mapInfo = tibble(
   sourceId = numeric(),
   sourceStart = numeric(),
   sourceEnd = numeric(),
-  sourceAdd = numeric())
+  sourceAdd = numeric(),
+  destStart = numeric(),
+  destEnd = numeric())
 
 seedSplit = str_split(input[1]," ")[[1]]
 seeds = as.numeric(seedSplit[2:length(seedSplit)])
@@ -29,7 +31,9 @@ for(line in input){
       sourceId = str_which(source, sourceList),
       sourceStart = match[2],
       sourceEnd = match[2] + match[3],
-      sourceAdd = match[1] - match[2]
+      sourceAdd = match[1] - match[2],
+      destStart = sourceStart + sourceAdd,
+      destEnd = sourceEnd + sourceAdd
     )
     mapInfo = mapInfo %>%
       add_row(newRow)
@@ -76,6 +80,88 @@ print(paste0("Part 1: ",minLoc))
 seedsLong = tibble(
   seedStart = seeds[seq(1,length(seeds),2)],
   seedNum = seeds[seq(2,length(seeds),2)]
-) 
-print(paste0("Don't want to go through ",sum(seedsLong$seedNum),
+)
+
+print(paste0("Nope...Can't check ",sum(seedsLong$seedNum),
              " seeds, so doing something else..."))
+
+
+# Function to use the map to turn a tibble of input ranges to 
+# a tibble of ranges for the next level up...
+mapRange = function(id = 0,
+                    inpRanges = NA){
+  outRanges = tibble(
+    rangeStart = numeric(),
+    rangeLen = numeric(),
+    mapId = numeric(),
+    inpId = numeric()
+  )
+  print(paste0("Mapping from ",id," to ",id+1))
+  # Get the map for this level
+  map = mapInfo %>%
+    filter(sourceId == id)
+  # Calculate for each input range
+  for(i in 1:nrow(inpRanges)){
+    # start and end of range we're mapping
+    rStart = inpRanges$rangeStart[i]
+    rLen = inpRanges$rangeLen[i]
+    rEnd = rLen + rStart
+    # Check every row of the map
+    for(j in 1:nrow(map)){
+      mStart = map$sourceStart[j]
+      mEnd = map$sourceEnd[j]
+      mapAdd = map$sourceAdd[j]
+      # Easiest case, inputrange resides fully in map range
+      if(mStart<=rStart & mEnd >= rEnd){
+        # Keeps the same range length, just modify start
+        outRanges = outRanges %>% add_row(
+          rangeStart = rStart + mapAdd,
+          rangeLen = rLen,
+          mapId = j,
+          inpId = i)
+        # don't need to look at other maps for this inpRange
+        break
+
+      # Range starts before, but overlaps map, starting at mStart
+      } else if(mStart>=rStart & mStart<=rEnd){
+        # Calculate length of new range
+        newLen = rEnd - mStart + 1
+        outRanges = outRanges %>% add_row(
+          rangeStart = mStart + mapAdd,
+          rangeLen = newLen,
+          mapId = j,
+          inpId = i
+        )
+        
+      # Range starts within, and ends after
+      } else if(rStart<=mEnd & rEnd>=mEnd){
+        newLen = mEnd - rStart + 1
+        outRanges = outRanges %>% add_row(
+          rangeStart = rStart + mapAdd,
+          rangeLen = newLen,
+          mapId = j,
+          inpId = i
+        )
+        
+      # Range starts before and ends after map
+      } else if(mStart>=rStart & mEnd<=rEnd){
+        outRanges = outRanges %>% add_row(
+          rangeStart = mStart + mapAdd,
+          rangeLen = mEnd-mStart+1,
+          mapId = j,
+          inpId = i
+        )
+      }
+      # That should be all the options
+    }
+  }
+  outRanges
+}
+
+rangesToMap = seedsLong %>%
+  rename(rangeStart = seedStart,
+         rangeLen = seedNum)
+for(k in 1:7){
+  rangesToMap = mapRange(k,rangesToMap)
+}
+
