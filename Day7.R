6ab42afedf66f9d971e964646159a004a9cb52dd
akingw <adam.king.w@gmail.com>
2023-12-09 14:06:41 -0700

    D7P2 complete

--- Day7.R
+++ Day7.R
@@ -10,6 +10,9 @@ library(tidyverse)
 
 cardOrder = c("A"=13,"K"=12,"Q"=11,"J"=10,"T"=9,"9"=8,
               "8"=7,"7"=6,"6"=5,"5"=4,"4"=3,"3"=2,"2"=1)
+
+cardOrder2 = c("A"=13,"K"=12,"Q"=11,"J"=1,"T"=10,"9"=9,
+              "8"=8,"7"=7,"6"=6,"5"=5,"4"=4,"3"=3,"2"=2)
 input = readLines("Day7Input.txt")
 numHands = length(input)
 handsTib = tibble(
@@ -57,4 +60,66 @@ handsTib = handsTib %>%
          winnings = rank * bet)
 
 print(paste0("Part 1: ",handsTib$winnings %>% sum()))
-# 250977165 too high
+
+# Part 2 tie breaking method still works. Need new logic for type assignment
+
+
+hands = handsTib %>% pull(handT)
+
+for(hand in hands){
+  handOld = hand
+  hand = gsub("J","",hand)
+  if(str_length(hand) == 0){
+    handType = 7
+  } else {
+    nj = str_length(handOld)-str_length(hand)
+    cards = str_split(hand,"")[[1]]
+    numCards = str_count(hand,cards)
+    numCardsU = numCards %>% unique()
+    handType = case_when(
+      # if no Jokers, same as before
+      nj == 0 ~ case_when(
+        max(numCards) == 5 ~ 7,
+        max(numCards) == 4 ~ 6,
+        max(numCards) == 3 ~ case_when(
+          sum(numCardsU) == 5 ~ 5,
+          T ~ 4),
+        max(numCards) == 2 ~ case_when(
+          sum(numCards) == 9 ~ 3,
+          T ~ 2),
+        T ~ 1),
+      nj == 1 ~ case_when(
+        max(numCards) == 1 ~ 2,
+        max(numCards) == 2 ~ case_when(
+          sum(numCards) == 8 ~ 5,
+          T ~ 4),
+        max(numCards) == 3 ~ 6,
+        T ~ 7),
+      nj == 2 ~ case_when(
+        max(numCards) == 1 ~ 4,
+        max(numCards) == 2 ~ 6,
+        T ~ 7),
+      nj == 3 ~ case_when(
+        max(numCards) == 1 ~ 6,
+        T ~ 7),
+      # 4 or 5 get 5x
+      T ~ 7) 
+  }
+  handsTib = handsTib %>%
+    mutate(type = case_when(
+      handT == handOld ~ handType,
+      T ~ type
+    ))
+}
+handsTib = handsTib %>%
+  mutate(c1 = cardOrder2[str_sub(handT,1,1)],
+         c2 = cardOrder2[str_sub(handT,2,2)],
+         c3 = cardOrder2[str_sub(handT,3,3)],
+         c4 = cardOrder2[str_sub(handT,4,4)],
+         c5 = cardOrder2[str_sub(handT,5,5)])
+handsTib = handsTib %>%
+  arrange(-type,-c1,-c2,-c3,-c4,-c5) %>%
+  mutate(rank = nrow(handsTib)-row_number()+1,
+         winnings = rank * bet)
+
+print(paste0("Part 2: ",handsTib$winnings %>% sum()))
