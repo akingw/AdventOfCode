3b422243e0faa9a49dc7869826dfff780fc7b951
akingw <adam.king.w@gmail.com>
2023-12-11 14:37:37 -0700

    D10P2 Done

--- Day10.R
+++ Day10.R
@@ -6,12 +6,12 @@ map = str_split(input,"")
 letToDir = function(map = map){
   dirs <<- matrix(nrow = length(map),
                   ncol = length(map[[1]]))
-  y = 0
+  r = 0
   for(line in map){
-    y = y + 1
-    x = 0
+    r = r + 1
+    c = 0
     for(elem in line){
-      x = x + 1
+      c = c + 1
       dir = case_when(
         elem == '|' ~ "UD",
         elem == '-' ~ "LR",
@@ -22,23 +22,23 @@ letToDir = function(map = map){
         elem == 'S' ~ "S",
         T ~ '.'
       )
-      dirs[x,y] <<- dir
-      if(elem == 'S'){strtInd <<- c(x,y)}
+      dirs[r,c] <<- dir
+      if(elem == 'S'){strtInd <<- c(r,c)}
     }
   }
 }
 
 findConnect = function(strt = strtInd){
   conns = vector(mode='list',length = 2)
-  x = strt[1]
-  y = strt[2]
+  r = strt[1]
+  c = strt[2]
   touches = rbind(
     # Coordinates of adjacent, and the direction that sends pipe to start.
     # 4th is the directio we'd 'hit' index from if starting at S
-    c(x-1,y,"R","L"),
-    c(x+1,y,"L","R"),
-    c(x,y-1,"U","D"),
-    c(x,y+1,"D","U"))
+    c(r,c-1,"R","L"),
+    c(r,c+1,"L","R"),
+    c(r-1,c,"U","D"),
+    c(r+1,c,"D","U"))
   for(i in nrow(touches)){
     touch = touches[i,]
     if(grepl(touch[3],
@@ -52,30 +52,85 @@ findConnect = function(strt = strtInd){
 
 followPath = function(strt = strt){
   atS = F
-  x = as.numeric(strt[1])
-  y = as.numeric(strt[2])
-  pth <<- c(x,y)
+  r = as.numeric(strt[1])
+  c = as.numeric(strt[2])
+  pth <<- c(r,c)
   dirFrom = strt[3]
   while(!atS){
-    locDirs = dirs[x,y]
+    locDirs = dirs[r,c]
     if(locDirs == "S"){break}
     nextStep = sub(dirFrom,"",locDirs)
     if(nextStep == "D"){
-      y = y + 1
+      r = r + 1
       dirFrom = "U"
     } else if(nextStep == "U"){
-      y = y - 1
+      r = r - 1
       dirFrom = "D"
     } else if(nextStep == "L"){
-      x = x - 1
+      c = c - 1
       dirFrom = "R"
     } else if(nextStep == "R"){
-      x = x + 1
+      c = c + 1
       dirFrom = "L"
     } else {
-      print("Got to ",x,", ",y,". Shouldn't be here...")
+      print(paste0("Got to ",c,", ",r,". Shouldn't be here..."))
     }
-    pth <<- rbind(pth,c(x,y))
+    pth <<- rbind(pth,c(r,c))
+  }
+}
+
+findWithin = function(pth = pth){
+  inPth <<- 0
+  # first remove non-path elements
+  pthOnly <<- matrix(data = ".",
+                   nrow = nrow(dirs),
+                   ncol = ncol(dirs))
+  for(i in 1:nrow(pth)){
+    r = pth[i,1]
+    c = pth[i,2]
+    pthOnly[r,c] <<- dirs[r,c]
+    if(pthOnly[r,c] == "S"){
+      pthOnly[r,c] = "UD"
+    }
+  }
+  inRow <<- matrix(data=0,nrow=140,ncol = 1)
+  for(r in 1:nrow(pthOnly)){
+    # Reset at start of each row, can't start inside.
+    nCross = 0
+    goUp = F
+    goDn = F
+    rowNum = 0
+    line = pthOnly[r,]
+    for(c in 1:length(line)){
+      elem = line[c]
+      if(elem == "UD"){
+        nCross = nCross + 1
+      } else if(grepl("U",elem)){
+        if(goUp){
+          goUp = F
+        } else {
+          goUp = T
+        }
+      } else if(grepl("D",elem)){
+        if(goDn){
+          goDn = F
+        } else {
+          goDn = T
+        }
+      }
+      if(goDn & goUp){
+        nCross = nCross + 1
+        goDn = F
+        goUp = F
+      }
+      if(elem == "."){
+        if((nCross %% 2) == 1){
+          inPth <<- inPth + 1
+          rowNum = rowNum + 1
+        }
+      }
+    }
+    inRow[r,1] <<- rowNum
   }
 }
 
@@ -89,3 +144,43 @@ findConnect(strtInd)
 followPath(strtAt)
 
 print(paste0("Part 1: ",nrow(pth)/2))
+
+# Part 2.
+# Scan across the whole area, and detect when we cross the path
+# can tell if in or outside of path based on number of crosses.
+findWithin(pth)
+
+## Comenting out some troubleshooting I had to do
+## Issue ended up being that I didn't handle "S", so offset the number of 
+## times crosing.
+## prntPth = matrix(data = ".",
+##                 nrow = 140,
+##                 ncol = 140)
+
+## for(r in 1:nrow(pthOnly)){
+  line = pthOnly[r,]
+  for(c in 1:length(line)){
+    elem = pthOnly[r,c]
+    prnt = case_when(
+      elem == "UD" ~ "|",
+      elem == "UR" ~ "L",
+      elem == "UL" ~ "J",
+      elem == "DR" ~ "F",
+      elem == "DL" ~ "7",
+      elem == "LR" ~ "-",
+      elem == "S" ~ "S",
+      T ~ ".")
+    prntPth[r,c] = prnt
+  }
+}
+## l2 = ""
+## for(i in 1:nrow(prntPth)){
+  l1 = paste0(prntPth[i,],collapse="")
+  l2 = paste0(l2,"\n",l1,": ",inRow[i,1])
+}
+## writeLines(l2,"Day10Within.txt")
+
+print(paste0("Part 2: ",inPth))
+
+#589 Too high
+#1787 too
