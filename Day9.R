8aca875e81f22ac6de3e82111d151253a1648a04
akingw <adam.king.w@gmail.com>
2023-12-11 07:23:00 -0700

    Day9Part2

--- Day9.R
+++ Day9.R
@@ -35,8 +35,18 @@ find_next = function(tree = matrix()){
   # like the example says.
   sum(lasts)
 }
-
+find_prev = function(tree = matrix()){
+  nr = nrow(tree)
+  last_row = tree[nr,]
+  rev_last = rev(last_row)
+  val = 0
+  for(elem in rev_last){
+    val = elem - val
+  }
+  val
+}
 sum_new = 0
+sum_2 = 0
 ind = 0
 for(line in input){
   ind = ind + 1
@@ -45,14 +55,17 @@ for(line in input){
   dTrees[[ind]] = diff_tree
   # Find the next value for the input
   tree_next = find_next(diff_tree)
+  tree_prev = find_prev(diff_tree)
   sum_new = sum_new + tree_next
+  sum_2 = sum_2 + tree_prev
   
   # Print info for spot checking
-  print(paste0("Next for ",ind," is ",tree_next,
-               ". New tot is ",sum_new))
+  #print(paste0("Next for ",ind," is ",tree_next,
+  #             ". New tot is ",sum_new))
 }
 
 print(paste0("Part 1: ",sum_new))
 #1980766090 too high - incorrectly using max
 #1974913982 too high - didn't zero out sum before running
 #1974913326 too high
+print(paste0("Part 2: ",sum_2))
